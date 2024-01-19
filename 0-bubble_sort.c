2f2072c9bdd8d921e220ad137b8879af65b1aef3
kirlos basta <kirlosbasta@gmail.com>
2024-01-19 13:58:16 +0200

    Add bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+
+/**
+ * bubble_sort - sort a list of integer using bubble sort
+ * @array: Array of integer
+ * @size: Size of the array
+ * 
+ * Return: Nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, stat;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		stat = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+				stat = 1;
+			}
+		}
+		if (stat == 0)
+		{
+			break;
+		}
+	}
+}
+
+/**
+ * swap - Swap Two integer
+ * @num_1: integer 1
+ * @num_2: integer 2
+ *
+ * Return: Nothing  
+ */
+void swap(int *num_1, int *num_2)
+{
+	int tmp;
+
+	tmp = *num_1;
+	*num_1 = *num_2;
+	*num_2 = tmp;
+}
